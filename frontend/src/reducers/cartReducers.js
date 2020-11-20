f0787a9621be19ad80ed4762143de5a67cde8d3f
David Ladipo <64803207+deedevs@users.noreply.github.com>
2020-11-20 15:13:17 +0200

    Add to cart functionality

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -5,13 +5,13 @@ export const cartReducer = (state = { cartItems: [] }, action) => {
     case CART_ADD_ITEM:
       const item = action.payload;
 
-      const existItem = state.cartItem.find((x) => x.product === item.product);
+      const existItem = state.cartItems.find((x) => x.product === item.product);
 
       if (existItem) {
         return {
           ...state,
-          cartItems: state.cartItems.map(
-            (x => x.product === existItem.product ? item : x)
+          cartItems: state.cartItems.map((x) =>
+            x.product === existItem.product ? item : x
           ),
         };
       } else {
